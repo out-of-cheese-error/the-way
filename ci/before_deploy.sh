8e8f1605359e226b4503e7cc24b932e619cef501
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-07-23 16:27:53 +0200

    fix cross

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # Update this to build the artifacts that matter to you
-    cross rustc --bin the-way --target $TARGET --release -- -C lto
+    cross rustc --bin the-way --target $TARGET --release
 
     # Update this to package the right artifacts
     cp target/$TARGET/release/the-way $stage/
