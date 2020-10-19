f1fbb68a0241840d8f952057e5aaaf7114b52e73
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-10-19 21:33:18 +0200

    fix(color_eyre): don't display env message on error
    This removes the "Backtrace omitted.
    Run with RUST_BACKTRACE=1
    environment variable to display it.
    Run with RUST_BACKTRACE=full to
    include source snippets." message from color_eyre.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use the_way::language::get_languages;
 use the_way::the_way::{cli::TheWayCLI, TheWay};
 
 fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
+    color_eyre::config::HookBuilder::blank()
+        .display_env_section(false)
+        .install()?;
     let languages_yml = include_str!("languages.yml");
     let languages = get_languages(languages_yml)?;
     let cli = TheWayCLI::from_args();
